9a2adce25b4c48bc7dd7667c0da4c708fe4aa1d3
hopliteFA <94584530+hopliteFA@users.noreply.github.com>
2022-10-15 19:57:36 -0400

    Added misses counter

--- day_7.py
+++ day_7.py
@@ -8,13 +8,19 @@ def get_guess():
     return guess
 
 def check_guess(guess):
-    print(f"\nYou guessed the letter {guess}:\n")
     counter = 0
-    for letter in secret_word:
-        if letter == guess:
-            print("\nHIT")
-            current_guessed[counter] = guess
-        counter +=1 
+    global misses #must declare as global to change from a function
+
+    print(f"\nYou guessed the letter {guess}:\n")
+    if guess in secret_word:
+        for letter in secret_word:
+            if letter == guess:
+                print("\nHIT")
+                current_guessed[counter] = guess
+            counter +=1 
+    else:
+        misses -= 1
+
     return current_guessed
 
 ##################
@@ -24,6 +30,7 @@ def check_guess(guess):
 #set guess to enter the while loop and the guessed letters
 guess = 'y'
 guessed_letters = []
+misses = 10
 
 #generate a random word
 word_list = ['potato', 'sword', 'generator']
@@ -48,9 +55,13 @@ while guess != 'q':
     print(current_guessed)
     guessed_letters.append(guess)
     print(f"\nYour guessed letters {guessed_letters}")
+    print(f"\nMisses remaining: {misses}")
 
     if "".join(current_guessed) == secret_word:
         print("\n\nYOU WON!!!\n\n")
         break
+    
+    if misses == 0:
+        print("Game Over!  Better luck next time.")
 
 
